16809d6d40c2db4bec147a74693e9149d59fb0e8
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 20:01:24 -0700

    Update Bary.java

--- Histogramy/Bary.java
+++ Histogramy/Bary.java
@@ -1,37 +1,30 @@
-/*    */ package Histogramy;
-/*    */ 
-/*    */ import java.awt.Color;
-/*    */ import java.awt.Graphics;
-/*    */ 
-/*    */ public class Bary
-/*    */ {
-/*    */   int altitude;
-/*    */   double xCoord;
-/*    */   double yCoord;
-/*    */   double width;
-/*    */   String number;
-/*    */ 
-/*    */   public Bary(double x, double y, double w, int a, String num)
-/*    */   {
-/* 12 */     this.altitude = a;
-/* 13 */     this.xCoord = x;
-/* 14 */     this.yCoord = y;
-/* 15 */     this.width = w;
-/* 16 */     this.number = num;
-/*    */   }
-/*    */   public void paint(Graphics g) {
-/* 19 */     g.setColor(Color.BLACK);
-/* 20 */     if (this.altitude != 0) {
-/* 21 */       g.drawRect((int)this.xCoord, (int)this.yCoord, (int)this.width, this.altitude);
-/*    */     }
-/* 23 */     g.setColor(Color.BLUE);
-/* 24 */     g.fillRect((int)this.xCoord, (int)this.yCoord, (int)this.width, this.altitude);
-/* 25 */     g.setColor(Color.CYAN);
-/* 26 */     g.drawString(this.number, (int)(this.xCoord + this.width / 2.0D), (int)(this.yCoord + this.altitude / 2));
-/*    */   }
-/*    */ }
+package Histogramy;
 
-/* Location:           /Users/timcoker/Desktop/Histogram.jar
- * Qualified Name:     Histogramy.Bary
- * JD-Core Version:    0.6.0
- */
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Bary {
+  int altitude;
+  double xCoord;
+  double yCoord;
+  double width;
+  String number;
+
+  public Bary(double x, double y, double w, int a, String num)  {
+    this.altitude = a;
+    this.xCoord = x;
+    this.yCoord = y;
+    this.width = w;
+    this.number = num;
+  }
+  public void paint(Graphics g) {
+    g.setColor(Color.BLACK);
+    if (this.altitude != 0) {
+      g.drawRect((int)this.xCoord, (int)this.yCoord, (int)this.width, this.altitude);
+    }
+    g.setColor(Color.BLUE);
+    g.fillRect((int)this.xCoord, (int)this.yCoord, (int)this.width, this.altitude);
+    g.setColor(Color.CYAN);
+    g.drawString(this.number, (int)(this.xCoord + this.width / 2.0D), (int)(this.yCoord + this.altitude / 2));
+  }
+}
